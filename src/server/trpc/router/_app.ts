841595b6527b098b13b843d898d3f78f1ccd957b
ZikXewen <datasup12@gmail.com>
2022-12-25 02:06:56 -0600

    implement index

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,8 +1,21 @@
-import { router } from "../trpc";
-import { exampleRouter } from "./example";
+import { publicProcedure, router } from "../trpc";
 
 export const appRouter = router({
-  example: exampleRouter,
+  getAll: publicProcedure.query(({ ctx }) =>
+    ctx.prisma.player.findMany({
+      orderBy: {
+        score: "desc",
+      },
+    })
+  ),
+  getTopThree: publicProcedure.query(({ ctx }) =>
+    ctx.prisma.player.findMany({
+      orderBy: {
+        score: "desc",
+      },
+      take: 3,
+    })
+  ),
 });
 
 // export type definition of API
